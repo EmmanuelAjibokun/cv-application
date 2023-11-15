3a2175af077cfcc65e25fba65936b11a0a5b78b7
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2023-11-15 15:43:23 +0100

    styled Personal component

--- src/components/personal/Personal.jsx
+++ src/components/personal/Personal.jsx
@@ -5,41 +5,43 @@ export default function Personal({ val, setVal }) {
   return (
     <div className="container">
       <h2>Personal Details</h2>
-      <div>
-        <div>
-          <p>Full name</p>
+      <form action="#">
+        <div className="user-details">
+          <div>
+            <p>Full name</p>
+            <input
+              type="text" 
+              value= {val.person}
+              onChange={(e)=>setVal.setPerson(e.target.value)}
+              placeholder="first and last name" required />
+            
+          </div>
+          <div>
+          <p>Email <span>recommended</span></p>
           <input 
             type="text" 
-            value= {val.person}
-            onChange={(e)=>setVal.setPerson(e.target.value)}
-            placeholder="first and last name" />
-          
+            value={val.mail}
+            onChange={(e) => setVal.setMail(e.target.value)}
+            placeholder="Enter email address" required />
+          </div>
+          <div>
+          <p>Phone number <span>recommended</span></p>
+          <input 
+            type="text" 
+            value={val.num}
+            onChange={(e) => setVal.setNum(e.target.value)}
+            placeholder="Enter phone number" required />
+          </div>
+          <div>
+          <p>Address <span>recommended</span></p>
+          <input 
+            type="text" 
+            value={val.addr}
+            onChange={e => setVal.setAddr(e.target.value)}
+            placeholder="City, Country" required />
+          </div>
         </div>
-        <div>
-        <p>Email <span>recommended</span></p>
-        <input 
-          type="text" 
-          value={val.mail}
-          onChange={(e) => setVal.setMail(e.target.value)}
-          placeholder="Enter email address" />
-        </div>
-        <div>
-        <p>Phone number <span>recommended</span></p>
-        <input 
-          type="number" 
-          value={val.num}
-          onChange={(e) => setVal.setNum(e.target.value)}
-          placeholder="Enter phone number" />
-        </div>
-        <div>
-        <p>Address <span>recommended</span></p>
-        <input 
-          type="text" 
-          value={val.addr}
-          onChange={e => setVal.setAddr(e.target.value)}
-          placeholder="City, Country" />
-        </div>
-      </div>
+      </form>
     </div>
   )
 }
