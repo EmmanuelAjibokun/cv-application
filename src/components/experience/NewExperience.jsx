d0c41630e8335d22c4d29c867d754444af8a6806
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2023-11-15 16:50:00 +0100

    styled newexperience component by adding fitting class name to component

--- src/components/experience/NewExperience.jsx
+++ src/components/experience/NewExperience.jsx
@@ -4,54 +4,57 @@ import Cancel from "../Cancel"
 
 export default function NewExperience() {
   return (
-    <div>
-      <div>
-        <p>Company Name</p>
-        <input 
-          type="text" 
-          value="FUTA" 
-          placeholder="Enter Company Name" />
-      </div>
-      <div>
-        <p>Position Title</p>
-        <input 
-          type="text" 
-          value="" 
-          placeholder="Enter Position Title" />
-      </div>
-      <div>
-        <p>Start Date</p>
-        <input 
-          type="text" 
-          value=""
-          placeholder="Enter Start Date" />
-      </div>
-      <div>
-        <p>End Date</p>
-        <input 
-          type="text" 
-          value="" 
-          placeholder="Enter End Date" />
-      </div>
-      <div>
-        <p>Location <span>optional</span></p>
-        <input 
-          type="text" 
-          value="" 
-          placeholder="Enter Location" />
-      </div>
-      <div>
-        <p>Description <span>optional</span></p>
-        <textarea
-          rows={10}
-          value="" 
-          placeholder="Enter Location"></textarea>
-      </div>
+    <form action="#">
+      <div className="user-details">
+        <div>
+          <p>Company Name</p>
+          <input 
+            type="text" 
+            value="FUTA" 
+            placeholder="Enter Company Name" />
+        </div>
+        <div>
+          <p>Position Title</p>
+          <input 
+            type="text" 
+            value="" 
+            placeholder="Enter Position Title" />
+        </div>
+        <div>
+          <p>Start Date</p>
+          <input 
+            type="text" 
+            value=""
+            placeholder="Enter Start Date" />
+        </div>
+        <div>
+          <p>End Date</p>
+          <input 
+            type="text" 
+            value="" 
+            placeholder="Enter End Date" />
+        </div>
+        <div>
+          <p>Location <span>optional</span></p>
+          <input 
+            type="text" 
+            value="" 
+            placeholder="Enter Location" />
+        </div>
+        <div>
+          <p>Description <span>optional</span></p>
+          <textarea
+            rows={10}
+            value="" 
+            placeholder="Enter Location"></textarea>
+        </div>
+        <div className="buttons">
+          <Delete />
+          <Cancel />
+          <Save />
+        </div>
 
-      <Delete />
-      <Cancel />
-      <Save />
-
-    </div>
+      </div>
+    </form>
   )
 }
