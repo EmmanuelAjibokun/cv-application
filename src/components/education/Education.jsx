291670b4dbc1dd2608e36f59b8e5a9d7b37cb7f7
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2023-11-16 23:18:22 +0100

    implement dropdown effect on education component

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -4,16 +4,24 @@ import caret from '../../assets/caret.png'
 import './education.css'
 
 import { useState } from "react"
+// import {CSSTransition} from "react-transition-group"
 
 export default function Education({val, setVal}) {
-  const [isActive, setIsActive] = useState(false)
+  const [isActive, setIsActive] = useState(false);
+  const style = {
+    transform: isActive ? "rotate(180deg)": "",
+    transition: "transfrom 5000ms ease"
+  }
 
   return (
     <div className="container">
-      <div className="dropdown_header" onClick={() => setIsActive(!isActive)} >
+      <div className= "dropdown_header"
+        onClick={() => {
+          setIsActive(!isActive);
+        }} >
         <img src={education} alt="graduation hat icon" className="education" />
         <h2>Education</h2>
-        <img src={caret} alt="caret dropdown icon" className="caret" />
+        <img src={caret} alt="caret dropdown icon" className="caret" style={style} />
       </div>
       {isActive && <NewEducation val={val} setVal={setVal} />}
     </div>
