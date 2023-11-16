0d7a65854789c19185e42ee209d75c1d4b8a4754
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2023-11-16 23:42:26 +0100

    implement dropdown effect on experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,30 @@
 import NewExperience from "./NewExperience"
+import caret from '../../assets/caret.png'
+import briefcase from '../../assets/briefcase.png'
+
+import '../education/education.css'
+
+import { useState } from "react"
 
 export default function Experience () {
+  const [isActive, setIsActive] = useState(false);
+  const style = {
+    transform: isActive ? "rotate(180deg)": "",
+    transition: "transfrom 5000ms ease"
+  }
+
   return (
     <div className="container">
-      <h2>Experience<span></span></h2>
-      <NewExperience />
+      <div className= "dropdown_header"
+        onClick={() => {
+          setIsActive(!isActive);
+        }} 
+      >
+        <img src={briefcase} alt="graduation hat icon" className="education" />
+        <h2>Experience</h2>
+        <img src={caret} alt="caret dropdown icon" className="caret" style={style} />
+      </div>
+      {isActive && <NewExperience />}
     </div>
   )
 }
