1a33db239f4462ab3e1a122d5d738341786c09e7
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2023-11-16 19:46:45 +0100

    add icons for dropdown, and implemented dropdown without animation

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,10 +1,21 @@
 import NewEducation from "./NewEducation"
+import education from '../../assets/education.png'
+import caret from '../../assets/caret.png'
+import './education.css'
+
+import { useState } from "react"
 
 export default function Education({val, setVal}) {
+  const [isActive, setIsActive] = useState(false)
+
   return (
     <div className="container">
-      <h2>Education</h2>
-      <NewEducation val={val} setVal={setVal} />
+      <div className="dropdown_header" onClick={() => setIsActive(!isActive)} >
+        <img src={education} alt="graduation hat icon" className="education" />
+        <h2>Education</h2>
+        <img src={caret} alt="caret dropdown icon" className="caret" />
+      </div>
+      {isActive && <NewEducation val={val} setVal={setVal} />}
     </div>
   )
 }
