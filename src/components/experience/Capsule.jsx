1c9cbcf3897d82e795d5fadf41d6285f802a2052
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2023-12-02 18:23:48 +0100

    make ExperienceList match educationList, experiece list now displays company name

--- src/components/experience/Capsule.jsx
+++ src/components/experience/Capsule.jsx
@@ -2,12 +2,12 @@
 import { useRef } from "react"
 
 export default function Capsule({index, val, vals, setVals, display, setDisplay, cancelField}) {
-  const temporaryClickedEducationRef = useRef(val)
-  console.log(temporaryClickedEducationRef.current)
+  const temporaryClickedExperienceRef = useRef(val)
+  console.log(temporaryClickedExperienceRef.current)
   
   function handleClick() {
-    temporaryClickedEducationRef.current = val;
-    console.log("inside handle click", temporaryClickedEducationRef.current)
+    temporaryClickedExperienceRef.current = val;
+    console.log("inside handle click", temporaryClickedExperienceRef.current)
     const newVals = [...vals]
     setVals(newVals.map(item => val.id == item.id ? {...item, display: true} : {...item, display: false}))
     setDisplay(true)
@@ -15,7 +15,7 @@ export default function Capsule({index, val, vals, setVals, display, setDisplay,
 
   return (
     <div className="capsule">
-      {display || <p className="capsule-name" onClick={handleClick}>{val.school}</p>}
+      {display || <p className="capsule-name" onClick={handleClick}>{val.company}</p>}
       {/* {val.display && display && <NewEducation index={index} val={val} vals={vals} setVals={setVals} setDisplay={setDisplay} temporaryClickedEducation={temporaryClickedEducationRef.current} cancelField={cancelField} />} */}
     </div>
   )
--- src/components/experience/ExperienceList.jsx
+++ src/components/experience/ExperienceList.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react"
-// import Capsule from "./capsule"
+import Capsule from "./Capsule"
 
-export default function EducationList({vals, setVals}) {
+export default function ExperienceList({vals, setVals}) {
   const [display, setDisplay] = useState(false);
   // const [cancelField, setCancelField] = useState(false);
   const maxId =vals.length > 0 ? Math.max(...vals.map(item => item.id)): 1;
 
   return (
-    <div className="education-list">
-      {/* {vals.map(val => <Capsule key={val.id} index={vals.indexOf(val)} vals={vals} val={val} setVals={setVals} display={display} setDisplay={setDisplay} cancelField={{cancelField, setCancelField}} />)} */}
+    <div className="experience-list">
+      {vals.map(val => <Capsule key={val.id} index={vals.indexOf(val)} vals={vals} val={val} setVals={setVals} display={display} setDisplay={setDisplay} />)}
       {display || <button className="add-btn" onClick={()=>{
           // setCancelField(true);
           setDisplay(true);
